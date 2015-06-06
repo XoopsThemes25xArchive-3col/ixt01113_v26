ec2f57a59b2ece2562a66f3913d2d8e3128ddc92
mambax7 <mambax7@gmail.com>
2015-06-06 07:48:58 -0400

    Original files

---
+++ ixt01113_v26/xo-info.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01113
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01113
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: xo-info.php 2516 2010-03-14 15:46:18Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+return array(
+ "languages"   => array("main"),
+ "copyright"   => "&copy; IXThemes Project <http://ixthemes.org>",
+ "name"        => "ixt01113",
+ "description" => "XOOPS 2.0.1x, 2.3.x, 2.4.x and 2.5.x, fixed width 1000px, 3 columns, preheader, slides, ext.header, ext.footer, 2 control zones, concatenation of left and right columns",
+ "version"     => "2.6",
+ "format"      => "XOOPS",
+ "author"      => "IXThemes Project, support@ixthemes.org",
+ "demo"        => "http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01113",
+ "demo1"       => "http://xoops233demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo2"       => "http://xoops240demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo3"       => "http://xoops241demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo4"       => "http://xoops242demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo5"       => "http://xoops243demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo6"       => "http://xoops244demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "demo7"       => "http://xoops250demo.ixthemes.org/index.php?xoops_theme_select=ixt01113",
+ "url"         => "http://ixthemes.org",
+ "download"    => "http://ixthemes.com",
+ "W3C"         => "XHTML 1.0 Transitional - CSS 1/2/3",
+ "licence"     => "GPL",
+ "thumbnail"   => "shot.gif",
+ "screenshot"  => "screenshot.jpg",
+);
+?>
---
+++ ixt01113_v26/xoplugins/header_custom_block01.php
@@ -0,0 +1,39 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01113
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01113
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: header_custom_block01.php 2516 2010-03-14 15:46:18Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+$block = array(
+	'id'        => '2001',
+	'title'     => 'IXThemes Theme Framework 2.6',
+	'content'   => '
+	<div>
+	<ul>
+ <li>Sidebar style on/off switching</li>
+ <li>Preheader on/off switching</li>
+ <li>Extended header on/off switching</li>
+ <li>Left and right columns concatenation</li>
+	<!--
+ <li>Footer on/off switching</li>
+ <li>Two control zones on/off switching</li>
+ <li>Slides zone on/off switching</li>
+ <li>Columns layout settings ( LRC, LCR, CLR)</li>
+ <li>RSS link in header on/off switching</li>
+ <li>Cursor:pointer for buttons is added</li>
+ <li>The maximum height of a footer background is increased up to 600px</li>
+ <li>The width of columns in percentage</li>
+	-->
+ </ul>
+	</div>
+	',
+);
+$this->assign('fblock', $block);
+?>
